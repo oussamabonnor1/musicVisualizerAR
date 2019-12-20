e9bc834d51e8029dc00e7980d9448f3eeb3040c6
oussamabonnor1 <oussamabonnor@gmail.com>
2019-12-20 22:59:05 +0100

    fixing spawning issues

--- Assets/Scripts/AudioVisualizer.cs
+++ Assets/Scripts/AudioVisualizer.cs
@@ -7,34 +7,33 @@ public class AudioVisualizer : MonoBehaviour {
 	public AudioSource music;
 	public float multiplier;
 	public float circleRadius;
-	public float minCubeHeight;
+	public float minCubeHeight, cubeDimensions;
 	public GameObject cubePrefab;
 	public float[] samples = new float[512];
 	public GameObject[] cubes;
 
-	public void Start(){
-	}
+	public void Start () { }
 
 	// Use this for initialization
 	public void found () {
-			for (int i = 0; i < cubes.Length; i++) {
-				if(!ImageTargetController.alreadyFound) cubes[i] = Instantiate (cubePrefab, transform.position, Quaternion.identity);
-				cubes[i].transform.parent = transform;
-				transform.localEulerAngles = new Vector3 (0, -((float) (360f / cubes.Length)) * i, 0);
-				cubes[i].transform.localPosition = transform.forward * circleRadius;
-				cubes[i].transform.LookAt (transform.up);
-			}
+		for (int i = 0; i < cubes.Length; i++) {
+			if (!ImageTargetController.alreadyFound) cubes[i] = Instantiate (cubePrefab, transform.position, Quaternion.identity);
+			cubes[i].transform.parent = transform;
+			transform.localEulerAngles = new Vector3 (0, -((float) (360f / cubes.Length)) * i, 0);
+			cubes[i].transform.localPosition = transform.forward * circleRadius;
+			cubes[i].transform.LookAt (transform.up);
+		}
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if(gameObject.activeSelf && ImageTargetController.found) getSamples ();
+		if (gameObject.activeSelf && ImageTargetController.found) getSamples ();
 	}
 
 	void getSamples () {
 		music.GetSpectrumData (samples, 0, FFTWindow.Blackman);
 		for (int i = 0; i < cubes.Length; i++) {
-			cubes[i].transform.localScale = new Vector3 (.02f, .02f, Mathf.Max (minCubeHeight, (float) (samples[i] * multiplier)));
+			cubes[i].transform.localScale = new Vector3 (cubeDimensions, cubeDimensions, Mathf.Max (minCubeHeight, (float) (samples[i] * multiplier)));
 		}
 	}
 }
